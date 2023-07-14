c41239d4c4b60133002214d0b686cfa1021f9ba2
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-07-14 10:28:38 -0700

    Day8

---
+++ w1/day8.js
@@ -0,0 +1,86 @@
+/*
+  Given a string that may have extra spaces at the start and the end,
+  return a new string that has the extra spaces at the start and the end trimmed (removed)
+  do not remove any other spaces.
+*/
+
+const str1 = "   hello world     ";
+const expected1 = "hello world";
+
+const str2 = "   hello    world     ";
+const expected2 = "hello    world";
+
+/**
+ * Trims any leading or trailing white space from the given str.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str
+ * @returns {string} The given string with any leading or trailing white space
+ *    stripped.
+ */
+function myTrim(str) {
+  let start = 0
+  let back = str.length - 1
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == " ") {
+      start++
+    }
+    else {
+      break
+    }
+
+  }
+  for (let j = str.length - 1; j >= 0; j--) {
+    if (str[j] == " ") {
+      back--
+    }
+    else {
+      break
+    }
+  }
+  let result = str.substring(start, back+1)
+  return result
+}
+
+console.log(myTrim(str1));
+console.log(myTrim(str2));
+
+/*****************************************************************************/
+
+/*
+  An anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+  typically using all the original letters exactly once.
+
+  Is there a quick way to determine if they aren't an anagram before spending more time?
+
+  Given two strings
+  return whether or not they are anagrams
+*/
+
+const two_strA1 = "yes";
+const two_strB1 = "eys";
+const two_expected1 = true;
+
+const two_strA2 = "yes";
+const two_strB2 = "eYs";
+const two_expected2 = true;
+
+const two_strA3 = "no";
+const two_strB3 = "noo";
+const two_expected3 = false;
+
+const two_strA4 = "silent";
+const two_strB4 = "listen";
+const two_expected4 = true;
+
+/**
+ * Determines whether s1 and s2 are anagrams of each other.
+ * Anagrams have all the same letters but in different orders.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} s1
+ * @param {string} s2
+ * @returns {boolean} Whether s1 and s2 are anagrams.
+ */
+function isAnagram(s1, s2) {}
