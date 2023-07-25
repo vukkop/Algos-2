9a7b139d0d44925c93856eb8766fb786e1f14577
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-07-25 09:03:08 -0700

    algos 13 day

---
+++ w1/day12.js
@@ -0,0 +1,57 @@
+/*
+  Recursively sum an arr of ints
+*/
+
+const two_nums1 = [1, 2, 3];
+const two_expected1 = 6;
+
+const two_nums2 = [1];
+const two_expected2 = 1;
+
+const two_nums3 = [];
+const two_expected3 = 0;
+
+/**
+ * Add params if needed for recursion
+ * Recursively sums the given array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @returns {number} The sum of the given nums.
+ */
+
+// normal function -> with a for/while loop
+function sumArr(nums) {}
+
+
+// recursive function
+function recursiveSumArr(nums) {}
+
+/*****************************************************************************/
+
+/*
+Recursive Sigma
+
+Input: integer
+Output: sum of integers from 1 to Input integer
+*/
+
+const num1 = 5;
+const expected1 = 15;
+// Explanation: (1+2+3+4+5)
+
+const num2 = 2.5;
+const expected2 = 3;
+// Explanation: (1+2)
+
+const num3 = -1;
+const expected3 = 0;
+
+/**
+ * Recursively sum the given int and every previous positive int.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} num
+ * @returns {number}
+ */
+function recursiveSigma(num) {}
