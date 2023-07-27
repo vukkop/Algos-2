714223187f93d306d926993516478a3978cfbb43
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-07-27 10:10:50 -0700

    Algos day 15

---
+++ w1/day15.js
@@ -0,0 +1,87 @@
+/*
+  Sum To One Digit
+  Implement a function sumToOne(num)​ that,
+  given a number, sums that number’s digits
+  repeatedly until the sum is only one digit. Return
+  that final one digit result.
+
+  Tips:
+    to access digits from a number, need to convert it .toString() to access each digit via index
+    parseInt(arg) returns arg parsed as an integer, or NaN if it couldn't be converted to an int
+    isNaN(arg) used to check if something is NaN
+*/
+
+const num1 = 5;
+const expected1 = 5;
+
+const num2 = 10;
+const expected2 = 1;
+
+const num3 = 25;
+const expected3 = 7;
+
+/**
+ * Sums the given number's digits until the number becomes one digit.
+ * @param {number} num The number to sum to one digit.
+ * @returns {number} One digit.
+ */
+function sumToOneDigit(num) {
+
+  const numStr = num.toString();
+
+  if (numStr.length === 1) {
+    return num;
+  } else {
+
+    let sum = 0;
+    for (let digit of numStr) {
+      sum += parseInt(digit);
+    }
+
+
+    return sumToOneDigit(sum);
+  }
+}
+
+console.log(sumToOneDigit(num1));
+console.log(sumToOneDigit(num2));
+console.log(sumToOneDigit(num3));
+
+/*****************************************************************************/
+
+/*
+  Recursive Binary Search
+
+  Input: SORTED array of ints, int value
+  Output: bool representing if value is found
+
+  Recursively search to find if the value exists, do not loop over every element.
+
+  Approach:
+  Take the middle item and compare it to the given value.
+  Based on that comparison, narrow your search to a particular section of the array
+*/
+
+const two_nums1 = [1, 3, 5, 6];
+const two_searchNum1 = 4;
+const two_expected1 = false;
+
+const two_nums2 = [4, 5, 6, 8, 12];
+const two_searchNum2 = 5;
+const two_expected2 = true;
+
+const two_nums3 = [3, 4, 6, 8, 12];
+const two_searchNum3 = 3;
+const two_expected3 = true;
+
+/**
+ * Add params if needed for recursion
+ * Recursively performs a binary search (divide and conquer) to determine if
+ * the given sorted nums array contains the given number to search for.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} sortedNums
+ * @param {number} searchNum
+ * @returns {boolean} Whether the searchNum was found in the sortedNums array.
+ */
+function binarySearch(sortedNums, searchNum) {}
