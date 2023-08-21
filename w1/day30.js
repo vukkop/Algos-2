5955913a553f8413c50b9663d223b94b44567dae
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-08-21 08:59:31 -0700

    Algos Day 30 fixed

--- w1/day30.js
+++ w1/day30.js
@@ -40,41 +40,38 @@ const expected6 = false;
  * @returns {boolean} Whether the given str can be rearranged into a palindrome.
  */
 function canStringBecomePalindrome(str) {
-  /**
-   * Determines whether or not it is possible for the string's characters to be
-   * rearranged into a palindrome.
-   * - Space: O(n).
-   * - Time: O(n).
-   * @param {string} str
-   * @returns {boolean} Whether the given str can be rearranged into a palindrome.
-   */
-  function canStringBecomePalindrome(str) {
-    if (str.length < 1) {
-      return false;
-    }
-    const charFrequency = new Map();
-
-    for (const char of str) {
-      charFrequency.set(char, (charFrequency.get(char) || 0) + 1);
-    }
-    let oddCount = 0;
-
-    for (const count in charFrequency.values()) {
-      if (count % 2 !== 0) {
-        oddCount++;
-      }
-    }
-
-    oddCount <= 1;
+  if (str.length == 0) return false;
+  let freqObj = {};
+  for (let i = 0; i < str.length; i++) {
+    if (freqObj.hasOwnProperty(str[i]) && freqObj[str[i]] == 1) {
+      freqObj[str[i]] = 0;
+    } else freqObj[str[i]] = 1;
+  }
+  let countOdd = 0;
+  for (key in freqObj) {
+    if (freqObj[key] == 1) countOdd++;
+  }
+  if (countOdd > 1) {
+    return false;
+  } else {
+    return true;
   }
 
-  console.log(canStringBecomePalindrome(str1)); // Expected: False
-  console.log(canStringBecomePalindrome(str2)); // Expected: True
-  console.log(canStringBecomePalindrome(str3)); // Expected: True
-  console.log(canStringBecomePalindrome(str4)); // Expected: True
-  console.log(canStringBecomePalindrome(str5)); // Expected: True
-  console.log(canStringBecomePalindrome(str6)); // Expected: False
+  // if (str.length % 2 == 1) {
+  //   if (countOdd > 1) return false;
+  // }
+  // else {
+  //   if (countOdd > 0) return false;
+  // }
+  // return true;
 }
+
+console.log(canStringBecomePalindrome(str1)); // Expected: False
+console.log(canStringBecomePalindrome(str2)); // Expected: True
+console.log(canStringBecomePalindrome(str3)); // Expected: True
+console.log(canStringBecomePalindrome(str4)); // Expected: True
+console.log(canStringBecomePalindrome(str5)); // Expected: True
+console.log(canStringBecomePalindrome(str6)); // Expected: False
 
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/drop-it
 
@@ -87,34 +84,42 @@ function canStringBecomePalindrome(str) {
   Return an empty array if the callback never returns true
 */
 
-// const nums1 = [1, 4, 3, 6, 2, 9, 15];
-// const callback1 = (elem) => {
-//   return elem > 5;
-// };
-// //const expected1 = [6, 2, 9, 15];
+const nums1 = [1, 4, 3, 6, 2, 9, 15];
+const callback1 = (elem) => {
+  return elem > 5;
+};
+//const expected1 = [6, 2, 9, 15];
 
-// const nums2 = [...nums1];
-// const callback2 = (elem) => {
-//   return elem > 2;
-// };
-// //const expected2 = [4, 3, 6, 2, 9, 15];
+const nums2 = [...nums1];
+const callback2 = (elem) => {
+  return elem > 2;
+};
+//const expected2 = [4, 3, 6, 2, 9, 15];
 
-// const nums3 = [...nums1];
-// const callback3 = (elem) => false; // a callback that always returns false
-// //const expected3 = [];
+const nums3 = [...nums1];
+const callback3 = (elem) => false; // a callback that always returns false
+//const expected3 = [];
 
-// /**
-//  * Removes every element in the array, starting from idx 0 until the callback
-//  * function returns true when passed the iterated element.
-//  * - Time: O(?).
-//  * - Space: O(?).
-//  * @param {Array<any>} arr
-//  * @callback cb A callback function that expects to receive an array element.
-//  * @returns {Array<any>} The given array with only the remaining items.
-//  */
+/**
+ * Removes every element in the array, starting from idx 0 until the callback
+ * function returns true when passed the iterated element.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<any>} arr
+ * @callback cb A callback function that expects to receive an array element.
+ * @returns {Array<any>} The given array with only the remaining items.
+ */
 
-// function dropIt(arr, cb) {}
+function dropIt(arr, cb) {
+  for (const x of arr) {
+    if (cb(x)) {
+      //[1, 4, 3, 6, 2, 9, 15];
+      return arr.slice(arr.indexOf(x));
+    }
+  }
+  return [];
+}
 
-// console.log(dropIt(nums1, callback1));
-// console.log(dropIt(nums2, callback2));
-// console.log(dropIt(nums3, callback3));
+console.log(dropIt(nums1, callback1));
+console.log(dropIt(nums2, callback2));
+console.log(dropIt(nums3, callback3));
