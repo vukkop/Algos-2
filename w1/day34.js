d5ce024dfc9e706f82c3f26194c74d114c7bca4f
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-08-25 08:08:45 -0700

    Algos Day 34

---
+++ w1/day34.js
@@ -0,0 +1,26 @@
+// sort (mergeSort, quickSort)
+// set/ hashmap / object -- frequency table
+// JS objects
+// 2 pointers
+
+/*
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+*/
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+const strs1 = ["eat", "tea", "tan", "ate", "nat", "bat"];
+const expected1 = [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]];
+
+const strs2 = [];
+const expected2 = [[""]];
+
+const strs3 = ["a"];
+const expected3 = [["a"]];
+
+function groupAnagrams(strs) {}
